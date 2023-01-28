431f93409d88cea0eaafccbf900e8f3e73668819
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-01-28 13:20:41 +0300

    update follow target for turret

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] float range = 1.5f;
-    [SerializeField] GameObject target;
+    [SerializeField] Collider target;
     [SerializeField] LayerMask enemyLayer;
 
     // Update is called once per frame
@@ -14,22 +15,33 @@ public class Turret : MonoBehaviour
     {
         UpdateTarget();
         UpdateRotation();
+        //ClearTarget();
     }
 
     void UpdateTarget()
     {
-        target = null;
         float closestDistance = Mathf.Infinity;
 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, enemyLayer);
+        if(hitColliders.Contains(target)) { return; }
         for (int i = 0; i < hitColliders.Length; i++)
         {
             float distance = Vector3.Distance(transform.position, hitColliders[i].transform.position);
             if (distance < closestDistance)
             {
                 closestDistance = distance;
-                target = hitColliders[i].gameObject;
+                target = hitColliders[i];
             }
+        }
+    }
+
+    void ClearTarget()
+    {
+        if (target == null) return;
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < range) {
+            target = null;
+            print("Tuan");
         }
     }
 
