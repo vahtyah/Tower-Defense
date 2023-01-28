011d38bd491cd37e523fac2c7da104190a3ed251
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-01-28 17:46:12 +0300

    add fire for turret

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 70f;
+    [SerializeField] int damage = 50;
+    Transform target;
+
+    public void setTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            ObjectPooler.instance.DeactivateObject(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    private void HitTarget()
+    {
+        ObjectPooler.instance.DeactivateObject(gameObject);
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,9 +6,16 @@ using UnityEngine.UIElements;
 
 public class Turret : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] float range = 1.5f;
     [SerializeField] Collider target;
     [SerializeField] LayerMask enemyLayer;
+
+    [Header("Use bulletGO")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    public float fireRate = 1f;
+    public float fireCountdown = 0f;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +23,27 @@ public class Turret : MonoBehaviour
         UpdateTarget();
         UpdateRotation();
         //ClearTarget();
+
+        if (fireCountdown <= 0f && target)
+        {
+            Shoot(bulletPrefab);
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot(GameObject bulletPrefab)
+    {
+        var bulletGO = ObjectPooler.instance.ActivateObject(bulletPrefab.tag);
+        if(bulletGO != null)
+        {
+            bulletGO.SetActive(true);
+            bulletGO.transform.position = firePoint.position;
+            bulletGO.transform.rotation = firePoint.rotation;
+            Bullet bullet = bulletGO.GetComponent<Bullet>();
+            if(bullet!=null) { bullet.setTarget(target.transform); }
+        }
     }
 
     void UpdateTarget()
@@ -24,6 +52,14 @@ public class Turret : MonoBehaviour
 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, enemyLayer);
         if(hitColliders.Contains(target)) { return; }
+        if(hitColliders.Length <= 0)
+        {
+            target = null;
+            return;
+            //nào bắt đầu wave thì cập nhật;
+            //tạo list enemies ở trong EnemyWayManager or ObjectPooling
+            //Tamj thoi thi nhu nay
+        }
         for (int i = 0; i < hitColliders.Length; i++)
         {
             float distance = Vector3.Distance(transform.position, hitColliders[i].transform.position);
