4cb0d300f770f0d37bd0a74c9316d14d5b09b575
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-03 23:24:15 +0300

    add scroll mouse

---
+++ Assets/Scenes/UILevelSelectForm.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILevelSelectForm : MonoBehaviour
+{
+    public MouseScroll mouseScroll;
+
+    private void Start()
+    {
+        mouseScroll.Init();
+    }
+}
