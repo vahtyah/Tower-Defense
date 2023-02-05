73ef0003a36f6f4a151f82deca5dd216b47e0895
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-05 22:08:43 +0300

    update

--- Assets/Scripts/PickUpSystem.cs
+++ Assets/Scripts/PickUpSystem.cs
@@ -1,16 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PickUpSystem : MonoBehaviour
 {
-    BuildManager buildManager;
-    Turret turret;
+    //BuildManager buildManager;
+    //Turret turret;
     [SerializeField] Material tuan;
+    Material[] originalMaterial;
+    MeshRenderer meshRenderer;
 
     private void Start()
     {
-        buildManager = GetComponent<BuildManager>();
-        turret = buildManager.GetTurretToBuild();
+        //buildManager = GetComponent<BuildManager>();
+        //turret = buildManager.GetTurretToBuild();
+        originalMaterial = gameObject.GetComponent<MeshRenderer>().materials;
+       
+            for (int i = 0; i < originalMaterial.Length; i++)
+            {
+                print(originalMaterial[i].color);
+            }
+        
+        ChangeMaterials();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print(originalMaterial[0].color);
+        }
+    }
+
+    void ChangeMaterials()
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        for (int i = 0; i < renderer.materials.Length; i++)
+        {
+            renderer.materials[i].color = tuan.color;
+        }
+    }
+
+    void RestoreMaterials()
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        renderer.materials = originalMaterial;
     }
 }
