266f221e72b46aeadfb05d5d8eca1b31831c1341
Туан <8268826@gmail.com>
2024-05-31 13:54:47 +0300

    fix UIShop

---
+++ Assets/Scripts/UI/UIShop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShop : MonoBehaviour
+{
+    [SerializeField] Turret[] ballistaTurret;
+
+    BuildManager buildManager;
+    private void Start() { buildManager = BuildManager.instance; }
+
+    public void SelectBallista(string turretName)
+    {
+        foreach (var turret in ballistaTurret)
+        {
+            if (String.Compare(turret.gameObject.tag, turretName, StringComparison.Ordinal) == 0) continue;
+            buildManager.SelectTurretToBuild(turret);
+            break;
+        }
+    }
+}
